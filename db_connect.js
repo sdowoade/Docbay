bc0714861a6b4769ef8eb495102060422a0470d9
Owoade Dotun <dotun.owoade@andela.com>
2016-01-13 15:09:05 +0100

    Allow role access for updating documents

--- db_connect.js
+++ db_connect.js
@@ -1,7 +1,7 @@
 'use strict';
 var mongoose = require('mongoose'),
   autoIncrement = require('mongoose-auto-increment'),
-  db = 'mongodb://localhost:27017/hht';
+  db = 'mongodb://localhost:27017/dmsdb';
 
 mongoose.connect(db);
 autoIncrement.initialize(mongoose.connection);
