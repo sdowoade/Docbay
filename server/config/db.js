c77ea5a16022d1054631e167155d840096ffd274
Owoade Dotun <dotun.owoade@andela.com>
2016-02-20 00:28:42 +0100

    Add Coverage For Bend Tests Refers to [#114128401] Remove redundant Database url

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,10 @@
  */
 'use strict';
 var mongoose = require('mongoose'),
-  autoIncrement = require('mongoose-auto-increment'),
-  db = 'mongodb://localhost:27017/tsting45'; //process.env.DATABASE_URL;
+  autoIncrement = require('mongoose-auto-increment');
 
+require('dotenv').load();
+var db = process.env.DATABASE_URL;
 mongoose.connect(db);
 autoIncrement.initialize(mongoose.connection);
 module.exports = mongoose;
